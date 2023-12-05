7359cde4dfa299eb1ad421abeb1b09bee43c9185
Aadit Bagga <abagga1@uncc.edu>
2023-12-04 19:50:35 -0500

    -setup inital file organization

---
+++ connected-scatterplot.js
@@ -0,0 +1,10 @@
+// connected-scatterplot.js
+import * as d3 from 'https://d3js.org/d3.v7.js';
+
+// Add a function named 'main'
+function main() {
+    console.log("Hello World");
+}
+
+// Execute main when the window loads
+window.addEventListener('load', main);
