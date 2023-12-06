b8289026979f65231c895c261306f0dc0d7d22ae
Aadit Bagga <abagga1@uncc.edu>
2023-12-06 15:55:58 -0500

    -[feature] Time Range Highlighting

--- connected-scatterplot.js
+++ connected-scatterplot.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let svg, x, y, xAxis, yAxis, line, tooltip, zoom;
+    let svg, x, y, xAxis, yAxis, line, highlightLine, tooltip, zoom;
 
     function handleFileSelect(evt) {
         const file = evt.target.files[0];
@@ -22,87 +22,69 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const startYearInput = d3.select('#startYear');
         const endYearInput = d3.select('#endYear');
+        const startYearLabel = d3.select('#startYearLabel');
+        const endYearLabel = d3.select('#endYearLabel');
 
         startYearInput
             .attr('min', minYear)
-            .attr('max', maxYear - 1) // Ensure the start year is less than the end year
+            .attr('max', maxYear)
             .attr('value', minYear)
             .on('input', function() {
-                const endYear = endYearInput.property('value');
-                updateChart(data, this.value, endYear);
+                startYearLabel.text(this.value);
+                updateHighlight(data, this.value, endYearInput.property('value'));
             });
 
         endYearInput
-            .attr('min', minYear + 1) // Ensure the end year is more than the start year
+            .attr('min', minYear)
             .attr('max', maxYear)
             .attr('value', maxYear)
             .on('input', function() {
-                const startYear = startYearInput.property('value');
-                updateChart(data, startYear, this.value);
+                endYearLabel.text(this.value);
+                updateHighlight(data, startYearInput.property('value'), this.value);
             });
+
+        startYearLabel.text(startYearInput.property('value'));
+        endYearLabel.text(endYearInput.property('value'));
     }
 
-    function updateChart(data, startYear, endYear) {
-        const filteredData = data.filter(d => d.year >= startYear && d.year <= endYear);
-        redrawChart(filteredData);
+    function updateHighlight(data, startYear, endYear) {
+        const highlightedData = data.filter(d => d.year >= startYear && d.year <= endYear);
+        highlightLine
+            .datum(highlightedData)
+            .attr("d", line);
     }
 
     function initializeChart(data) {
-        // Set up dimensions and margins.
         const margin = { top: 20, right: 30, bottom: 30, left: 40 },
               width = 928 - margin.left - margin.right,
               height = 720 - margin.top - margin.bottom;
 
-        // Append the svg object to the body of the page
         svg = d3.select("#chart").append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom);
 
-        // Add a group element that contains the elements to be drawn.
         const g = svg.append("g")
             .attr("transform", `translate(${margin.left},${margin.top})`);
 
-        // Setup scales
         x = d3.scaleLinear().range([0, width]);
         y = d3.scaleLinear().range([height, 0]);
-        
-        // Define the line
-        line = d3.line()
-            .curve(d3.curveCatmullRom)
-            .x(d => x(d.miles))
-            .y(d => y(d.gas));
 
-        // Setup the axes.
         xAxis = g.append("g")
             .attr("class", "x axis")
             .attr("transform", `translate(0,${height})`);
         yAxis = g.append("g")
             .attr("class", "y axis");
 
-        // Define the zoom behavior.
+        line = d3.line()
+            .curve(d3.curveCatmullRom)
+            .x(d => x(d.miles))
+            .y(d => y(d.gas));
+
         zoom = d3.zoom()
             .scaleExtent([1, 40])
             .translateExtent([[-100, -100], [width + 90, height + 100]])
             .on("zoom", zoomed);
 
-        // Function to handle mouseover event
-        function mouseover(event, d) {
-            tooltip.transition()
-                .duration(200)
-                .style("opacity", .9);
-            tooltip.html("Year: " + d.year + "<br/>Miles: " + d.miles.toFixed(2) + "<br/>Cost: $" + d.gas.toFixed(2))
-                .style("left", (event.pageX) + "px")
-                .style("top", (event.pageY - 28) + "px");
-        }
-
-        // Function to handle mouseout event
-        function mouseout() {
-            tooltip.transition()
-                .duration(500)
-                .style("opacity", 0);
-        }
-
-        // Create a tooltip
         tooltip = d3.select("body").append("div")
             .attr("class", "tooltip")
             .style("opacity", 0)
@@ -117,7 +99,6 @@ document.addEventListener('DOMContentLoaded', function() {
             .style("border-radius", "8px")
             .style("pointer-events", "none");
 
-        // Apply the zoom behavior to the svg.
         svg.call(zoom);
 
         function resetZoom() {
@@ -128,23 +109,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
         svg.on("dblclick.zoom", resetZoom);
 
-        // Function that updates the chart when zoomed.
-        function zoomed(event) {
-            const { transform } = event;
-            const zx = transform.rescaleX(x).interpolate(d3.interpolateRound);
-            const zy = transform.rescaleY(y).interpolate(d3.interpolateRound);
-            g.selectAll('.line').attr("d", line);
-            g.selectAll('circle').attr("cx", d => zx(d.miles)).attr("cy", d => zy(d.gas));
-            g.selectAll('.label').attr("x", d => zx(d.miles)).attr("y", d => zy(d.gas));
-            xAxis.call(d3.axisBottom(zx));
-            yAxis.call(d3.axisLeft(zy));
-        }
-
-        // Load the data and draw the chart.
         x.domain(d3.extent(data, d => d.miles)).nice();
         y.domain(d3.extent(data, d => d.gas)).nice();
 
-        // Draw the line path.
         g.append("path")
             .datum(data)
             .attr("fill", "none")
@@ -155,7 +122,14 @@ document.addEventListener('DOMContentLoaded', function() {
             .attr("class", "line")
             .attr("d", line);
 
-        // Add the points and the events for tooltip display
+        highlightLine = g.append("path")
+            .attr("fill", "none")
+            .attr("stroke", "red")
+            .attr("stroke-width", 2.5)
+            .attr("stroke-linejoin", "round")
+            .attr("stroke-linecap", "round")
+            .attr("class", "highlight-line");
+
         g.selectAll("circle")
             .data(data)
             .join("circle")
@@ -168,7 +142,6 @@ document.addEventListener('DOMContentLoaded', function() {
             .on("mouseover", mouseover)
             .on("mouseout", mouseout);
 
-        // Draw the labels.
         g.selectAll(".label")
             .data(data)
             .join("text")
@@ -178,26 +151,22 @@ document.addEventListener('DOMContentLoaded', function() {
             .text(d => d.year)
             .attr("font-family", "sans-serif")
             .attr("font-size", 10)
-            .attr("opacity", 0) // Start the labels as invisible
+            .attr("opacity", 0)
             .each(function(d) {
-                // Position labels based on the 'side' property.
                 const t = d3.select(this);
                 t.attr("text-anchor", getAnchor(d.side))
                  .attr("dx", getDx(d.side))
                  .attr("dy", getDy(d.side));
             });
 
-        // Fade in the labels after the transition.
         g.selectAll(".label")
             .transition()
             .duration(5000)
             .attr("opacity", 1);
 
-        // Draw the axes.
         xAxis.call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0));
         yAxis.call(d3.axisLeft(y).ticks(null, "$.2f").tickSizeOuter(0));
 
-        // Label the axes.
         xAxis.append("text")
             .attr("fill", "currentColor")
             .attr("x", width)
@@ -212,50 +181,40 @@ document.addEventListener('DOMContentLoaded', function() {
             .attr("dy", "0.75em")
             .attr("text-anchor", "end")
             .text("Cost per gallon");
+
+        updateHighlight(data, d3.min(data, d => d.year), d3.max(data, d => d.year));
     }
 
-    function redrawChart(data) {
-        // Redraw the chart with the updated data range.
-        // This function will be similar to the drawChart function but will
-        // only update the elements that need to be redrawn based on the new data.
-
-        // Update the domains for the x and y scales
-        x.domain(d3.extent(data, d => d.miles)).nice();
-        y.domain(d3.extent(data, d => d.gas)).nice();
-
-        // Select and update the line path
-        const g = svg.select("g");
-        g.select(".line")
-            .datum(data)
-            .attr("d", line);
-
-        // Select and update the circles
-        g.selectAll("circle")
-            .data(data)
-            .join("circle")
-            .attr("cx", d => x(d.miles))
-            .attr("cy", d => y(d.gas))
-            .attr("r", 3);
-
-        // Select and update the labels
-        g.selectAll(".label")
-            .data(data)
-            .join("text")
-            .attr("class", "label")
-            .attr("x", d => x(d.miles))
-            .attr("y", d => y(d.gas))
-            .text(d => d.year);
-
-        // Update the axes
-        xAxis.call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0));
-        yAxis.call(d3.axisLeft(y).ticks(null, "$.2f").tickSizeOuter(0));
+    function zoomed(event) {
+        const { transform } = event;
+        const zx = transform.rescaleX(x).interpolate(d3.interpolateRound);
+        const zy = transform.rescaleY(y).interpolate(d3.interpolateRound);
+        svg.selectAll('.line').attr("d", line);
+        svg.selectAll('.highlight-line').attr("d", highlightLine.attr("d"));
+        svg.selectAll('circle').attr("cx", d => zx(d.miles)).attr("cy", d => zy(d.gas));
+        svg.selectAll('.label').attr("x", d => zx(d.miles)).attr("y", d => zy(d.gas));
+        xAxis.call(d3.axisBottom(zx));
+        yAxis.call(d3.axisLeft(zy));
     }
 
-    // Attach the file input change event to the handleFileSelect function
+    function mouseover(event, d) {
+        tooltip.transition()
+            .duration(200)
+            .style("opacity", .9);
+        tooltip.html("Year: " + d.year + "<br/>Miles: " + d.miles.toFixed(2) + "<br/>Cost: $" + d.gas.toFixed(2))
+            .style("left", (event.pageX) + "px")
+            .style("top", (event.pageY - 28) + "px");
+    }
+
+    function mouseout() {
+        tooltip.transition()
+            .duration(500)
+            .style("opacity", 0);
+    }
+
     document.getElementById('fileInput').addEventListener('change', handleFileSelect, false);
 });
 
-// Helper functions for label positioning based on the 'side' property.
 function getAnchor(side) {
     if (side === 'right') return "start";
     if (side === 'left') return "end";
